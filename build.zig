e0acb8425598804191be5bf425aaa8bbfb669888
torph <73564623+Torphedo@users.noreply.github.com>
2024-06-25 13:43:40 -0400

    Update to Zig 0.12.0, use freestanding target

--- build.zig
+++ build.zig
@@ -1,44 +1,82 @@
 const std = @import("std");
 
-pub fn build(b: *std.Build.Builder) void {
+var gpa = std.heap.GeneralPurposeAllocator(.{}){};
 
-    // Compile the ELF itself for aarch64 Linux
-    // elf2nro complains about the ELF file being bad if I compile for
-    // freestanding and disable libc... It also complains if I don't jump to
-    // _start, so right now I'm just adding a jump to start that's never hit
-    // because I jump to main first. So right now libc is unavailable, which I
-    // haven't really tried fixing yet. I might need to add a Switch OS tag or something.
-    const switch_target = std.zig.CrossTarget{ .os_tag = .linux, .cpu_arch = .aarch64 };
+pub fn system(cmd: []const u8) !void {
+    const allocator = gpa.allocator();
 
-    // Compile elf2nro for the current machine
-    const native_target = std.zig.CrossTarget{ .os_tag = null, .cpu_arch = null };
+    const result = try std.ChildProcess.run(.{
+        .allocator = allocator,
+        .argv = &[_][]const u8{cmd},
+    });
+    defer {
+        allocator.free(result.stdout);
+        allocator.free(result.stderr);
+    }
+}
+
+pub fn build_assembly_obj(src: []const u8, obj_name: []const u8) !void {
+    const cmd_base = "zig build-obj -target aarch64-freestanding -cflags -Qunused-arguments -o{s} -- {s}";
+    const size = cmd_base.len + src.len + obj_name.len;
+
+    const buffer = try gpa.allocator().alloc(u8, size);
+    defer gpa.allocator().free(buffer);
+
+    const cmd = try std.fmt.bufPrintZ(buffer, cmd_base, .{ obj_name, src });
+    system(cmd) catch |err| {
+        if (err == error.FileNotFound) {
+            // Really dirty hack to silently discard the error that doesn't
+            // seem to actually cause problems. Sorry, my coffee went cold when
+            // I was writing this.
+        }
+    };
+}
+
+pub fn build(b: *std.Build) !void {
+
+    // libc is unavailable, which I haven't really tried fixing yet.
+    const switch_target = b.resolveTargetQuery(.{
+        .cpu_arch = .aarch64,
+        .cpu_model = .{ .explicit = &std.Target.aarch64.cpu.cortex_a57 },
+        .os_tag = .freestanding,
+        .abi = .none,
+        .ofmt = .elf,
+    });
 
     const exe = b.addExecutable(.{
         .name = "zig-hos",
         .target = switch_target,
-        .link_libc = true,
-        .root_source_file = .{ .path = "src/main.c" },
+        .link_libc = false,
         .optimize = b.standardOptimizeOption(.{}),
     });
-    exe.addIncludePath(.{ .path = "ext/libnx/nx/include/switch" });
-    // exe.addCSourceFile(.{ .file = std.Build.LazyPath.relative("ext/libnx/nx/source/services/sm.c"), .flags = &.{} });
-    exe.addAssemblyFile(.{ .path = "ext/libnx/nx/source/kernel/svc.s" });
-    exe.addAssemblyFile(.{ .path = "src/nro_entry.s" });
-    exe.setLinkerScript(.{ .path = "src/set_base_addr.ld" });
+    exe.addCSourceFile(.{ .file = b.path("src/main.c"), .flags = &.{} });
+
+    // Adding the assembly file normally causes Clang error because of unused
+    // parameters. We have to use this helper to compile them separately...
+    // Nevermind this bug mysteriously vanished before my very eyes
+    // try build_assembly_obj(b.path("src/nro_entry.s").getPath(b), b.path("nro_entry.o").getPath(b));
+    // exe.addObjectFile(b.path("nro_entry.o"));
+    // try build_assembly_obj(b.path("ext/libnx/nx/source/kernel/svc.s").getPath(), b.path("svc.o").getPath());
+    // exe.addObjectFile(b.path("svc.o"));
+
+    exe.addIncludePath(b.path("ext/libnx/nx/include/switch"));
+    exe.addAssemblyFile(b.path("ext/libnx/nx/source/kernel/svc.s"));
+    exe.addAssemblyFile(b.path("src/nro_entry.s"));
+    exe.setLinkerScript(b.path("src/set_base_addr.ld"));
     b.installArtifact(exe);
 
     const elf2nro = b.addExecutable(.{
         .name = "elf2nro",
-        .root_source_file = .{ .path = "ext/switch-tools/src/elf2nro.c" },
         .link_libc = true,
-        .target = native_target,
+        .target = b.host,
     });
 
     // No idea why we can't just pass in normal strings anymore, but this is required now...
-    elf2nro.addCSourceFile(.{ .file = std.Build.LazyPath.relative("ext/switch-tools/src/romfs.c"), .flags = &.{} });
-    elf2nro.addCSourceFile(.{ .file = std.Build.LazyPath.relative("ext/switch-tools/src/filepath.c"), .flags = &.{} });
-    elf2nro.addIncludePath(.{ .path = "ext/switch-tools/src" });
-    elf2nro.addIncludePath(.{ .path = "include" });
+    elf2nro.addCSourceFile(.{ .file = b.path("ext/switch-tools/src/elf2nro.c"), .flags = &.{} });
+    elf2nro.addCSourceFile(.{ .file = b.path("ext/switch-tools/src/romfs.c"), .flags = &.{} });
+    elf2nro.addCSourceFile(.{ .file = b.path("ext/switch-tools/src/filepath.c"), .flags = &.{} });
+    elf2nro.addIncludePath(b.path("ext/switch-tools/src"));
+    elf2nro.addIncludePath(b.path("include"));
     b.installArtifact(elf2nro);
 
     const build_nro = b.addRunArtifact(elf2nro);
--- src/nro_entry.s
+++ src/nro_entry.s
@@ -7,12 +7,12 @@
 
 .section ".text.crt0","ax"
 
-.global __module_start
-__module_start:
-    b _pre_start
+.global _start
+_start:
+    b main
 
     // Put MOD0 offset in the NRO file.
-    .word __nx_mod0 - __module_start
+    .word __nx_mod0 - _start
 
     // MOD0 has to start after the NRO header and at a power of 2 offset
     .balign 0x80
@@ -28,18 +28,4 @@ __nx_mod0:
 
 // Put the rest of the code at the next 0x10 boundary just to make it neater
 .balign 0x10
-
-test_text:
-.ascii "Assembly printing test\n"
-.balign 0x10
-_pre_start:
-    adr X0, test_text
-    movz X1, 23
-    svc 0x27
-    
-    b main
-
-    # elf2nro complains about the ELF format if we don't branch to _start,
-    # so this is a dirty hack to link in libc even though we can't use it
-    b _start
 
